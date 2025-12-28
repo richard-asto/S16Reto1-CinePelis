8d4676d5a0d10b8028989ecc0550520b668a4f42
richard-asto <SV09681015@idat.pe>
2025-12-28 01:20:16 -0500

    3.- Codificacion del componente pelis.js con el listado de peliculas

---
+++ src/data/pelis.js
@@ -0,0 +1,122 @@
+export const pelis = [
+    {   /*--- PELI 1  ---*/
+        img: 'https://image.tmdb.org/t/p/w300/roKMM6S9DRGFNujmvqlk8l5YbN3.jpg',
+        titulo: 'Harry Potter y la piedra filosofal',
+        genero: 'Aventura, Fantasía',
+        descripcion:
+            'El día en que cumple once años, Harry Potter se entera de que es hijo de dos destacados hechiceros.',
+        id: 1,
+    },
+    {   /*--- PELI 2  ---*/
+        img: 'https://image.tmdb.org/t/p/w300/z9UUfYZzKVBCEFs6TnCdAp5mUbJ.jpg',
+        titulo: 'Willow',
+        genero: 'Acción, Aventura, Fantasía',
+        descripcion:
+            'Temerosa de que una profecía declare que una niña nacerá para provocar su caída.',
+        id: 2,
+    },
+    {   /*--- PELI 3  ---*/
+        img: 'https://image.tmdb.org/t/p/w300/qf5bntGx2p5kQKO7zUbHrbOx9pp.jpg',
+        titulo: 'Dune',
+        genero: 'Aventura, Ciencia ficción',
+        descripcion:
+            'Paul Atreides debe viajar al planeta más peligroso del universo.',
+        id: 3,
+    },
+    {   /*--- PELI 4  ---*/
+        img: 'https://image.tmdb.org/t/p/w300/uu6RaEAfkIQaolf20axWaRU4h3w.jpg',
+        titulo: 'Mi vecino Totoro',
+        genero: 'Animación, Fantasía',
+        descripcion:
+            'Dos chicas se mudan a una casa donde habitan espíritus del bosque.',
+        id: 4,
+    },
+    {   /*--- PELI 5  ---*/
+        img: 'https://image.tmdb.org/t/p/w300/6GkKzdNosVAL7UGgwTtCHSxLQ67.jpg',
+        titulo: 'Guardianes de la Galaxia',
+        genero: 'Acción, Ciencia ficción',
+        descripcion:
+            'Peter Quill debe reunir a su equipo para salvar el universo.',
+        id: 5,
+    },
+    {   /*--- PELI 6  ---*/
+        img: 'https://image.tmdb.org/t/p/w300/gh2bmprLtUQ8oXCSluzfqaicyrm.jpg',
+        titulo: 'John Wick',
+        genero: 'Acción, Crimen',
+        descripcion:
+            'Un exasesino regresa al mundo criminal para vengarse.',
+        id: 6,
+    },
+    {   /*--- PELI 7  ---*/
+		img: 'https://image.tmdb.org/t/p/w300/bqOqQ2Tawum3eHKNrc94P4EeaZB.jpg',
+		titulo: 'Avatar: El camino del agua',
+		genero: 'Acción, Aventura, Ciencia ficción',
+		descripcion:
+			'Más de una década después de los acontecimientos de Avatar, los Navi Jake Sully, Neytiri y sus hijos viven en paz en los bosques de Pandora hasta que regresan los hombres del cielo.',
+		id: 7,
+	},
+	{   /*--- PELI 8  ---*/
+		img: 'https://image.tmdb.org/t/p/w300/yTzr6Nc4RcTXVw9SsWQNJdcjTz5.jpg',
+		titulo: 'Aquaman',
+		genero: 'Acción, Aventura, Fantasía',
+		descripcion:
+			'Cuando Arthur Curry (Jason Momoa) descubre que es mitad humano y mitad atlante, emprenderá el viaje de su vida en esta aventura que no sólo le obligará a enfrentarse a quién es en realidad, sino también a descubrir si es digno de cumplir con su destino.',
+		id: 8,
+	},
+	{   /*--- PELI 9  ---*/
+		img: 'https://image.tmdb.org/t/p/w300/1r8TWaAExHbFRzyqT3Vcbq1XZQb.jpg',
+		titulo: 'Jurassic Park',
+		genero: 'Aventura, Ciencia ficción',
+		descripcion:
+			'El multimillonario John Hammond tiene una idea para un espectacular parque temático: una isla retirada donde los visitantes puedan observar dinosaurios reales.',
+		id: 9,
+	},
+    {   /*--- PELI 10  ---*/
+        img: 'https://www.cine-calidad.legal/uploads/mini/multi-poster/10/c4a7b36850f4d60ac8a4f5fe713060.jpg?2',
+        titulo: 'La Empleada',
+        genero: 'Misterio, Suspense',
+        descripcion:
+            'Una joven (Sydney Sweeney), con un pasado complicado comienza a trabajar como asistenta en la lujosa casa de los Winchester.',
+        id: 10,
+    },
+    {   /*--- PELI 11  ---*/
+        img: 'https://www.cine-calidad.legal/uploads/mini/multi-poster/4e/13bf6a269db5b8c7c0230143d749a6.jpg?2',
+        titulo: 'Anaconda',
+        genero: 'Aventura, Comedia, Terror',
+        descripcion:
+            'Doug y Griff, amigos de toda la vida, atraviesan una crisis de mediana edad y deciden realizar el remake de su película favorita de su juventud: Anaconda (1997).',
+        id: 11,
+    },
+    {   /*--- PELI 12  ---*/
+        img: 'https://www.cine-calidad.legal/uploads/mini/multi-poster/29/ad58ebb4191b1f9ce3df644bde4c7f.jpg?2',
+        titulo: 'Bob Esponja: En busca de los pantalones cuadrados',
+        genero: 'Familia, Comedia, Aventura, Animación',
+        descripcion:
+            'Bob Esponja se sumerge en lo más profundo del océano para probar su valor persiguiendo al Holandés Errante.',
+        id: 12,
+    },
+    {   /*--- PELI 13  ---*/
+        img: 'https://www.cine-calidad.legal/uploads/mini/multi-poster/e8/805632b16213fde401e3e70f1402c5.jpg?2',
+        titulo: 'Zootopia 2',
+        genero: 'Animación, Familia, Comedia, Aventura, Misterio, Top',
+        descripcion:
+            'Para evitar ir al programa de "Compañeros en Crisis", los policías Judy Hopps y Nick Wilde aceptan realizar el caso de la llegada de un reptil a Zootopia, poniéndo a prueba su relación.',
+        id: 13,
+    },
+    {   /*--- PELI 14  ---*/
+        img: 'https://www.cine-calidad.legal/uploads/mini/multi-poster/b3/5a7ba6c673708d26e9449987b97c3b.jpg?2',
+        titulo: 'El Grinch',
+        genero: 'Animación, Familia, Comedia',
+        descripcion:
+            'Illumination y Universal Pictures presentan El Grinch, basada en el amado clásico del Dr. Seuss.',
+        id: 14,
+    },
+    {   /*--- PELI 15  ---*/
+        img: 'https://www.cine-calidad.legal/uploads/mini/multi-poster/e7/b4f7f8ad918f26a35dc1e86a3d88bb.jpg?2',
+        titulo: 'Nuremberg: El Juicio Del Siglo',
+        genero: 'Historia, Drama, Suspense, Bélica',
+        descripcion:
+            'Basado en el libro "El nazi y el psiquiatra" de Jack El-Hai.',
+        id: 15,
+    },
+];
